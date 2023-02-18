9281766787c3ba58e57ae3de32c807ac81cd91d8
Duarte Dias <dudias@microsoft.com>
2023-02-18 11:58:12 -0800

    cleanup code

--- AzureFunctions/BuyLowSellHigh/__init__.py
+++ AzureFunctions/BuyLowSellHigh/__init__.py
@@ -3,66 +3,57 @@ import datetime
 import logging
 import azure.functions as func
 
-# Custom imports
+# Alpaca api imports
 from alpaca.data import StockHistoricalDataClient
 from alpaca.data.timeframe import TimeFrame
 from alpaca.data.requests import StockBarsRequest
 from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import GetAssetsRequest, MarketOrderRequest
 from alpaca.trading.enums import AssetClass, OrderSide, TimeInForce
+
+# Other imports
 import os
 
-# Large cap stocks
-large_caps = ['AAPL', 'AMZN', 'MSFT', 'GOOG', 'GOOGL', 'FB', 'TSLA', 'NVDA', 'PYPL', 'ADBE', 'NFLX', 'AVGO', 'CSCO', 'INTC', 'AMGN', 'QCOM', 'TXN', 'COST', 'MDLZ', 'SBUX', 'BIIB', 'TMUS', 'ISRG', 'CHTR', 'BKNG']
-target_stock = 'TSLA'
-
-def check_price_dip(ticker):
-    # Get the latest price data for the stock
-    stock_price = api.get_last_trade(ticker).price
-    
-    # Get the historical data for the stock
-    historical_data = api.get_aggs(ticker, 1, 'day')
-    
-    # Calculate the average price of the stock over the past 25 days
-    avg_price = sum(data.close for data in historical_data) / 25
-    
-    # Check if the current price is less than the average price
-    if stock_price < avg_price:
-        return True
-    else:
-        return False
+# Set target stock
+target_stock = "MSFT"
 
 def main(mytimer: func.TimerRequest) -> None:
+    # Get the current time
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
 
+    # Get environment variables and initialize clients
     api_key_id = os.getenv("API_KEY_ID")
     secret_access_key = os.getenv("SECRET_ACCESS_KEY")
     historical_data_client = StockHistoricalDataClient(api_key_id, secret_access_key)
     trading_client = TradingClient(api_key_id, secret_access_key, paper=True)
 
+    # Get historical data for a stock
+    # Preparing request query
     request_params = StockBarsRequest(
         symbol_or_symbols=[target_stock],
         timeframe=TimeFrame.Day,
         start="2022-02-01 00:00:00"
     )
-
+    # Submit request
     bars = historical_data_client.get_stock_bars(request_params)
+
+    # Convert to pandas dataframe
     bars_df = bars.df
     logging.info(bars_df)
 
     # TODO: Perform fancy analysis on the data
 
-    # Get all current assets
+    # Get all current assets in our alpaca trading account
     assets = trading_client.get_all_positions()
     logging.info(assets)
 
-    # Submit a market order to buy 1 share of the stock
-    # preparing order
+    # Submit a market order to buy half a share of the target stock
+    # Preparing order
     market_order_data = MarketOrderRequest(
-                        symbol="MSFT",
+                        symbol=target_stock,
                         qty=0.5,
                         side=OrderSide.BUY,
                         time_in_force=TimeInForce.DAY

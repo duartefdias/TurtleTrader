ce7c5a39efabdb9238d744261daa4534d0f12e08
Duarte Dias <dudias@microsoft.com>
2023-03-08 14:57:40 -0800

    start work on adding chart

--- LiveTradesViewerApp/src/App.tsx
+++ LiveTradesViewerApp/src/App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { StockChart } from './components/StockChart';
+
+export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <StockChart />
       </header>
     </div>
   );
 }
 
-export default App;
---
+++ LiveTradesViewerApp/src/components/StockChart.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//https://apexcharts.com/react-chart-demos/line-charts/zoomable-timeseries/
+
+export const StockChart = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Chart
+      </header>
+    </div>
+  );
+}
